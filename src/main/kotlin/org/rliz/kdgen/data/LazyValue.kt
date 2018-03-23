aeeb2995664eaf3d09beddf40c191df302453694
Constantin Weißer <i7c@posteo.de>
2018-03-23 16:55:09 +0100

    More flexible transformations
    Move transformations of LazyValues to the interface.
    
    Introduce infix operator "with" to combine two LazyValues into a pair,
    that can be transformed together.

--- src/main/kotlin/org/rliz/kdgen/data/LazyValue.kt
+++ src/main/kotlin/org/rliz/kdgen/data/LazyValue.kt
@@ -1,7 +1,20 @@
 package org.rliz.kdgen.data
 
+import org.rliz.kdgen.data.primitive.LazyExpression
+
 interface LazyValue<out T : Any> {
 
     fun eval(): T
 
+    infix fun <O : Any> transform(transformation: (T) -> O): LazyValue<O> =
+            LazyExpression({ transformation(this.eval()) })
+
+    infix fun <O : Any> flatTransform(transformation: (T) -> LazyValue<O>) =
+            LazyExpression({ transformation(this.eval()).eval() })
+
+    infix fun <I2 : Any> with(that: LazyValue<I2>) = Pair(this, that)
+
 }
+
+infix fun <I1 : Any, I2 : Any, O : Any> Pair<LazyValue<I1>, LazyValue<I2>>.transform(transformation: (I1, I2) -> O) =
+        LazyExpression({ transformation(this.first.eval(), this.second.eval()) })
--- src/main/kotlin/org/rliz/kdgen/data/primitive/Transformation.kt
+++
@@ -1,9 +0,0 @@
-package org.rliz.kdgen.data.primitive
-
-import org.rliz.kdgen.data.LazyValue
-
-infix fun <I : Any, O : Any> LazyValue<I>.transform(transformation: (I) -> O): LazyValue<O> =
-        LazyExpression({ transformation(this.eval()) })
-
-infix fun <I : Any, O : Any> LazyValue<I>.flatTransform(transformation: (I) -> LazyValue<O>) =
-        LazyExpression({ transformation(this.eval()).eval() })
