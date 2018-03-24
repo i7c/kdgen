cdba47eca885611127eaf8d44078479b4a76fcfb
Constantin Weißer <i7c@posteo.de>
2018-03-24 16:08:19 +0100

    Update city list and move to resource file
    There were some broken city names. We now use a list of international
    cities. The list is moved to a text file in the resources, which makes
    maintenance easier and the code files smaller.

--- src/main/kotlin/org/rliz/kdgen/common/Helper.kt
+++ src/main/kotlin/org/rliz/kdgen/common/Helper.kt
@@ -1,3 +1,7 @@
 package org.rliz.kdgen.common
 
+import java.io.File
+
 infix fun <T : Any> Int.times(generator: () -> T): List<T> = (1..this).map { generator() }.toList()
+
+fun getResourceFile(path: String) = File(ClassLoader.getSystemClassLoader().getResource(path).file)
