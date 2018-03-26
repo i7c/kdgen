39dfec179e0e485407c2927788ba52af3c0c4341
Constantin Weißer <i7c@posteo.de>
2018-03-26 10:58:20 +0200

    Use class loader of local class

--- src/main/kotlin/org/rliz/kdgen/common/Helper.kt
+++ src/main/kotlin/org/rliz/kdgen/common/Helper.kt
@@ -2,6 +2,8 @@ package org.rliz.kdgen.common
 
 import java.io.File
 
+class Helper
+
 infix fun <T : Any> Int.times(generator: () -> T): List<T> = (1..this).map { generator() }.toList()
 
-fun getResourceFile(path: String) = File(ClassLoader.getSystemClassLoader().getResource(path).file)
+fun getResourceFile(path: String) = File(Helper::class.java.classLoader.getResource(path).file)
