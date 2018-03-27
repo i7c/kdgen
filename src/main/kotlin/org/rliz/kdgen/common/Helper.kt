04bc3f79e7ac8ac2d8ce3f1384d2e66eb2d9d5ad
Constantin Weißer <i7c@posteo.de>
2018-03-28 00:12:40 +0200

    Really fix file loader this time
    Also works in linking applications now.

--- src/main/kotlin/org/rliz/kdgen/common/Helper.kt
+++ src/main/kotlin/org/rliz/kdgen/common/Helper.kt
@@ -1,9 +1,10 @@
 package org.rliz.kdgen.common
 
-import java.io.File
+import org.apache.commons.io.IOUtils
 
 class Helper
 
 infix fun <T : Any> Int.times(generator: () -> T): List<T> = (1..this).map { generator() }.toList()
 
-fun getResourceFile(path: String) = File(Helper::class.java.classLoader.getResource(path).file)
+fun resourceFileLines(path: String): List<String> =
+        (IOUtils.readLines(Helper::class.java.classLoader.getResourceAsStream(path)) as List<String>?)!!
--- src/main/kotlin/org/rliz/kdgen/data/simple/City.kt
+++ src/main/kotlin/org/rliz/kdgen/data/simple/City.kt
@@ -1,6 +1,6 @@
 package org.rliz.kdgen.data.simple
 
-import org.rliz.kdgen.common.getResourceFile
+import org.rliz.kdgen.common.resourceFileLines
 import org.rliz.kdgen.data.LazyValue
 import org.rliz.kdgen.data.primitive.CountingInt
 import org.rliz.kdgen.state.Counter
@@ -12,6 +12,6 @@ class City(private val countingInt: CountingInt = CountingInt(c)) : LazyValue<St
     companion object {
         private val c = Counter()
 
-        private val cities = getResourceFile("data/cities.txt").readLines()
+        private val cities = resourceFileLines("data/cities.txt")
     }
 }
